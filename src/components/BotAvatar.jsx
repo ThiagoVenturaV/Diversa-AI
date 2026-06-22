51734edcd0d8ee718a42fd16c448849c031f0b56
thiagoventurav <thiago2013ventura@gmail.com>
2026-06-22 20:25:52 -0300

    frontzin

---
+++ src/components/BotAvatar.jsx
@@ -0,0 +1,7 @@
+export default function BotAvatar() {
+  return (
+    <div className="chat-header-logo" style={{ marginTop: '2px' }}>
+      <i className="fa-solid fa-star text-white" style={{ fontSize: '11px' }} />
+    </div>
+  )
+}
